bfb4231c545362c5442cb9031af65c49bf8de836
Tawhid Dwin Swadhin <96833637+swadhin503@users.noreply.github.com>
2025-01-13 07:52:23 +0600

    Update login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -43,7 +43,7 @@ export default function Login() {
           const res = await createUserAction(userInfo);
           const data = await res.json();
           await update({
-            role: data?.role || 'user',
+            role: data?.data?.role || 'user',
           });
         } catch (error) {}
         replace('/');
